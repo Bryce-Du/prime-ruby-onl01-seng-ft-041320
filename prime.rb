02808d8411c2d159d0f7e4a9235b78d721c57485
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 15:10:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@ def prime? (num)
   return false if num%2 == 0
   attempts = (3..Integer.sqrt(num)).to_a
   odds = attempts.select {|odd| odd %2 != 0}
-  return odds.any? {num % odd != 0)
+  return false if odds.any? {num % odd == 0)
+  return true
 end
 
 puts prime?(27)
