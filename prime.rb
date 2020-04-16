8a906a8de27a6ddad7755e160e43094f8c746730
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 14:59:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime? (num)
   return false if num%2 == 0
   attempts = (3..Integer.sqrt(num)).to_a
-  odds = attempts.select {|odd| odd %2 == 0}
+  odds = attempts.select {|odd| odd %2 != 0}
   puts odds
 end
 
