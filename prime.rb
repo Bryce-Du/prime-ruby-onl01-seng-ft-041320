89be6170eeb99af222cab40207ae4404fec1c8de
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 15:18:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime? (num)
-  return true if num == 1 || num == 2
-  return false if num%2 == 0 || num.abs != num
+  return true if num == 2
+  return false if num%2 == 0 || num.abs != num || num == 1
   attempts = (3..Integer.sqrt(num)).to_a
   odds = attempts.select {|odd| odd %2 != 0}
   if odds.any? {|odd| num % odd == 0}
